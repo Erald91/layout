6905e4ee54ca966404f5f18d27972f76fca301ee
eraldcafka <erald.c@scopicsoftware.com>
2019-03-23 10:02:42 +0100

    + Added styling for FirstDiv container

--- film/src/components/Main/FirstDiv/index.js
+++ film/src/components/Main/FirstDiv/index.js
@@ -4,23 +4,24 @@ import AddMovies from '../AddMovies/index';
 
 class FirstDiv extends Component {
   constructor(props) {
-      super(props)
-      
+      super(props);
   }
 
-    render() {
-        return(
-           <div className="FirstDiv">
-              <div id="title"><h2>TITLES</h2></div>
-              <div className="search">
-                <input id="searchbox" type="text" placeholder="Search by Title,ID, Studio or Release Year" /> 
-                </div>
-              <div>
-                <input id="titlebox" type="submit" value="ADD TITLE" />
-              </div>
-           </div>
-        );
-    }
+  render() {
+    return(
+      <div className="FirstDiv">
+        <div className="FirstDiv_title-container">
+          <div id="title"><h2>TITLES</h2></div>
+          <div className="search">
+            <input id="searchbox" type="text" placeholder="Search by Title,ID, Studio or Release Year" /> 
+          </div>
+        </div>
+        <div className="FirstDiv_actions-container">
+          <button class="add-button" id="addTitleButton" data-target="#myModal">ADD TITLE</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default FirstDiv;
